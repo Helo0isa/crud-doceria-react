c2fec7e032b6f91fc3d582d36ea40dbb243d1bf8
Heloisa Avelino Mazon <heloisaavelinomazon@gmail.com>
2026-08-09 22:39:18 -0300

    Cadastro basico de doces funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 import docesIniciais from './data/doces.json'
 import ListaDoces from './components/ListaDoces.jsx'
+import FormDoce from './components/FormDoce.jsx'
 
 function App() {
   const [doces, setDoces] = useState(docesIniciais)
+
+  function adicionarDoce(novoDoce) {
+    setDoces([...doces, novoDoce])
+  }
 
   return (
     <div>
       <h1>Doceria</h1>
       <ListaDoces doces={doces} />
+      <FormDoce aoAdicionar={adicionarDoce} />
     </div>
   )
 }
+
+
 
 export default App
--- src/components/FormDoce.jsx
+++ src/components/FormDoce.jsx
@@ -1,40 +1,58 @@
 import { useState } from 'react'
 
-function FormDoce() {
+function FormDoce({aoAdicionar}) {
   const [nome, setNome] = useState('')
   const [sabor, setSabor] = useState('')
   const [preco, setPreco] = useState('')
   const [quantidade, setQuantidade] = useState('')
 
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const novoDoce = {
+        id: Date.now(),
+        nome: nome,
+        sabor: sabor,
+        preco: Number(preco),
+        quantidade: Number(quantidade)
+
+    }
+
+    aoAdicionar(novoDoce)
+  }
+
+
   return (
-    <form>
-        <input
+    <form onSubmit={handleSubmit}>
+        <input //Nome do doce
           type="text"
           placeholder="Nome"
           value={nome}
           onChange={(e) => setNome(e.target.value)}
         />
       
-        <input
+        <input //Sabor do doce
           type="text"
           placeholder="Sabor"
           value={sabor}
           onChange={(e) => setSabor(e.target.value)}
         />
       
-        <input
+        <input //Preço do doce
           type="number"
           placeholder="Preço"
           value={preco}
           onChange={(e) => setPreco(e.target.value)}
         />
     
-        <input
+        <input //Quantidade do doce
           type="number"
           placeholder="Quantidade"
           value={quantidade}
           onChange={(e) => setQuantidade(e.target.value)}
         />
+
+        <button type="submit">Cadastrar</button>
     </form>
   )
 }
